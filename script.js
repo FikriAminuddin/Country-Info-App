76d516c2a2c6c4144b11dbcefc0ed6b60854839b
FikriAminuddin <muhammad.fikri.aminuddin.fauzi@possimo.com>
2024-08-18 18:29:26 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,67 @@
+let searchBtn = document.getElementById("search-btn");
+let countryInp = document.getElementById("country-inp");
+searchBtn.addEventListener("click", () => {
+  let countryName = countryInp.value;
+  let fields = "name,capital,flags,continents,currencies,languages,population";
+  let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true&fields=${fields}`;
+  console.log(finalURL);
+  fetch(finalURL)
+    .then((response) => response.json())
+    .then((data) => {
+      let capital = data[0].capital[0];
+      let googleMapsURL = `https://www.google.com/maps/search/?api=1&query=${capital}`;
+
+      result.innerHTML = `
+        <img src="${data[0].flags.svg}" class="flag-img">
+        <h2>${data[0].name.common}</h2>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Capital:</h4>
+                <span>${capital}</span>
+            </div>
+        </div>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Continent:</h4>
+                <span>${data[0].continents[0]}</span>
+            </div>
+        </div>
+         <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Population:</h4>
+                <span>${data[0].population}</span>
+            </div>
+        </div>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Currency:</h4>
+                <span>${
+                  data[0].currencies[Object.keys(data[0].currencies)].name
+                } - ${Object.keys(data[0].currencies)[0]}</span>
+            </div>
+        </div>
+         <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Common Languages:</h4>
+                <span>${Object.values(data[0].languages)
+                  .toString()
+                  .split(",")
+                  .join(", ")}</span>
+            </div>
+        </div>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Google Maps Location:</h4>
+                <a href="${googleMapsURL}" target="_blank">View ${capital} on Google Maps</a>
+            </div>
+        </div>
+      `;
+    })
+    .catch(() => {
+      if (countryName.length == 0) {
+        result.innerHTML = `<h3>The input field cannot be empty</h3>`;
+      } else {
+        result.innerHTML = `<h3>Please enter a valid country name.</h3>`;
+      }
+    });
+});
